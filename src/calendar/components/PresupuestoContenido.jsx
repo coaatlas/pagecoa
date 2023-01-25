c27c0a4518c55c90f0ea485192f6c7aaae06e34e
leonardo arena <leonardofarena@gmail.com>
2023-01-25 19:20:22 -0300

    nn

--- src/calendar/components/PresupuestoContenido.jsx
+++ src/calendar/components/PresupuestoContenido.jsx
@@ -40,6 +40,11 @@ export const PresupuestoContenido = () => {
         twelve: 0,
         thirteen: 0,
         fourteen: 0,
+        fifteen: 0,
+        sixteen: 0,
+        seventeen: 0,
+        eighteen: 0,
+        nineteen: 0,
     })
    const [suma, setSuma] = useState()
  
@@ -58,6 +63,11 @@ export const PresupuestoContenido = () => {
             twelve,
             thirteen,
             fourteen,
+            fifteen,
+            sixteen,
+            seventeen,
+            eighteen,
+            nineteen
         } = number
      setSuma(Number(one) + 
              Number(two) + 
@@ -72,7 +82,12 @@ export const PresupuestoContenido = () => {
              Number(eleven) +
              Number(twelve) +
              Number(thirteen) +
-             Number(fourteen)
+             Number(fourteen) +
+                Number(fifteen) +
+                Number(sixteen) +
+                Number(seventeen) +
+                Number(eighteen)  +
+                Number(nineteen)
       )
    },[number])
  
@@ -85,15 +100,9 @@ export const PresupuestoContenido = () => {
   return (
    <>
    <Pdf targetRef={ref} filename="Presupuesto.pdf" 
-        options={{
-           
+        options={{           
             unit: 'in',
-            format: [  10, 21 ],     
-    
-            x: 0,
-            y: 0,
-        
-          
+            format: [  10, 21 ]        
 
          } } >
         {({ toPdf }) => <button       className="btn btn-primary fabb" onClick={toPdf}>Generar PDF</button>}
@@ -128,35 +137,33 @@ export const PresupuestoContenido = () => {
             <input
              type="text"
              name="nombre"
-             placeholder="........" 
+             placeholder="Nombre del cliente" 
              className="form-control-lgg border-0"
              required 
             
              /></p>
 
              <hr  />
-           
-           
-
-        
-                <hr  />
-
-           
-            <hr  />
+             <br />
            <p>Piso vinilico en rollo <sup>( x m<sup>2</sup> )</sup> $ : <input className="form-control-lg border-0" onChange={handleInput} name="one" value={number.one} placeholder="Ingesa monto"/></p>
            <p>Pulido e imprimación <sup>( x m<sup>2</sup> )</sup> $ :<input className="form-control-lg border-0"    onChange={handleInput} name="two" value={number.two} placeholder="Ingesa monto"/></p>
-           <p>Capa dos manos<sup>( x m<sup>2</sup> )</sup> $ :<input className="form-control-lg border-0"           onChange={handleInput} name="three" value={number.three} placeholder="Ingesa monto"/></p>
+           <p>Baldosas de pvc<sup>( x m<sup>2</sup> )</sup> $ :<input className="form-control-lg border-0"           onChange={handleInput} name="three" value={number.three} placeholder="Ingesa monto"/></p>
            <p>Capa dos manos<sup>( x m<sup>2</sup> )</sup> $ :<input className="form-control-lg border-0"           onChange={handleInput} name="four" value={number.four} placeholder="Ingesa monto"/></p>
-           <p>Capa dos manos<sup>( x m<sup>2</sup> )</sup> $ :<input className="form-control-lg border-0"           onChange={handleInput} name="five" value={number.five} placeholder="Ingesa monto"/></p>
-           <p>Capa dos manos<sup>( x m<sup>2</sup> )</sup> $ :<input className="form-control-lg border-0"           onChange={handleInput} name="six" value={number.six} placeholder="Ingesa monto"/></p>
-           <p>Capa dos manos<sup>( x m<sup>2</sup> )</sup> $ :<input className="form-control-lg border-0"           onChange={handleInput} name="seven" value={number.seven} placeholder="Ingesa monto"/></p>
-           <p>Capa dos manos<sup>( x m<sup>2</sup> )</sup> $ :<input className="form-control-lg border-0"           onChange={handleInput} name="eight" value={number.eight} placeholder="Ingesa monto"/></p>
-           <p>Capa dos manos<sup>( x m<sup>2</sup> )</sup> $ :<input className="form-control-lg border-0"           onChange={handleInput} name="nine" value={number.nine} placeholder="Ingesa monto"/></p>
-           <p>Capa dos manos<sup>( x m<sup>2</sup> )</sup> $ :<input className="form-control-lg border-0"           onChange={handleInput} name="ten" value={number.ten} placeholder="Ingesa monto"/></p>
-           <p>Capa dos manos<sup>( x m<sup>2</sup> )</sup> $ :<input className="form-control-lg border-0"           onChange={handleInput} name="eleven" value={number.eleven} placeholder="Ingesa monto"/></p>
-           <p>Capa dos manos<sup>( x m<sup>2</sup> )</sup> $ :<input className="form-control-lg border-0"           onChange={handleInput} name="twelve" value={number.twelve} placeholder="Ingesa monto"/></p>
-           <p>Capa dos manos<sup>( x m<sup>2</sup> )</sup> $ :<input className="form-control-lg border-0"           onChange={handleInput} name="thirteen" value={number.thirteen} placeholder="Ingesa monto"/></p>
-           <p>Capa dos manos<sup>( x m<sup>2</sup> )</sup> $ :<input className="form-control-lg border-0"           onChange={handleInput} name="fourteen" value={number.fourteen} placeholder="Ingesa monto"/></p>
+           <p>Capa una manos<sup>( x m<sup>2</sup> )</sup> $ :<input className="form-control-lg border-0"           onChange={handleInput} name="five" value={number.five} placeholder="Ingesa monto"/></p>
+           <p>Trabajo minimo ( 20 m<sup>2</sup> ) gl $ :<input className="form-control-lg border-0"           onChange={handleInput} name="six" value={number.six} placeholder="Ingesa monto"/></p>
+           <p>Instlación vinilico en pared<sup>( x m<sup>2</sup> )</sup> $ :<input className="form-control-lg border-0"           onChange={handleInput} name="seven" value={number.seven} placeholder="Ingesa monto"/></p>
+           <p>Adaptador para zocalo sanit ml $ :<input className="form-control-lg border-0"           onChange={handleInput} name="eight" value={number.eight} placeholder="Ingesa monto"/></p>
+           <p>Zocalo sanitario ml $ :<input className="form-control-lg border-0"           onChange={handleInput} name="nine" value={number.nine} placeholder="Ingesa monto"/></p>
+           <p>Soldadura ml $ :<input className="form-control-lg border-0"           onChange={handleInput} name="ten" value={number.ten} placeholder="Ingesa monto"/></p>
+           <p>Malla cobre ml $ :<input className="form-control-lg border-0"           onChange={handleInput} name="eleven" value={number.eleven} placeholder="Ingesa monto"/></p>
+           <p>Trabajo en interior del pais % $ :<input className="form-control-lg border-0"           onChange={handleInput} name="twelve" value={number.twelve} placeholder="Ingesa monto"/></p>
+           <p>Descolocación de piso de goma/otros<sup>( x m<sup>2</sup> )</sup> $ :<input className="form-control-lg border-0"           onChange={handleInput} name="thirteen" value={number.thirteen} placeholder="Ingesa monto"/></p>
+           <p>Colocación de varillas ml $ :<input className="form-control-lg border-0"           onChange={handleInput} name="fourteen" value={number.fourteen} placeholder="Ingesa monto"/></p>
+           <p>Listones Lvt pegados ml $ :<input className="form-control-lg border-0"           onChange={handleInput} name="fifteen" value={number.fifteen} placeholder="Ingesa monto"/></p>
+           <p>Pvc en rollo con 2 manos de masa niveladora <sup>( x m<sup>2</sup> )</sup> $ :<input className="form-control-lg border-0"           onChange={handleInput} name="sixteen" value={number.sixteen} placeholder="Ingesa monto"/></p>
+           <p>Goma en rollo 2 manos de masa niveladora <sup>( x m<sup>2</sup> )</sup> $ :<input className="form-control-lg border-0"           onChange={handleInput} name="seventeen" value={number.seventeen} placeholder="Ingesa monto"/></p>
+           <p>Goma en rollo<sup>( x m<sup>2</sup> )</sup> gl $ :<input className="form-control-lg border-0"           onChange={handleInput} name="eighteen" value={number.eighteen} placeholder="Ingesa monto"/></p>
+           <p>Pvc en rollo<sup>( x m<sup>2</sup> )</sup> gl $ :<input className="form-control-lg border-0"           onChange={handleInput} name="nineteen" value={number.nineteen} placeholder="Ingesa monto"/></p>
 
 
             <hr />
