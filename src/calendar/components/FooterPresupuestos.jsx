cfdbbff96971c2d44d63b3e627d15478c10858bd
leonardo arena <leonardofarena@gmail.com>
2023-02-28 22:04:44 -0300

    nnn

--- src/calendar/components/FooterPresupuestos.jsx
+++ src/calendar/components/FooterPresupuestos.jsx
@@ -13,7 +13,7 @@ export const FooterPresupuestos = () => {
          
       <div >
                 <span   style={{textAlign: 'left', color: 'black', fontSize: '11px' }}>Los precios son sujetos a cambios sin previo aviso</span> <br/>
-                <span   style={{textAlign: 'left', color: 'black', fontSize: '11px' }}>Los precios no incluyen IVA</span> <br/>
+                
                 <span   style={{textAlign: 'left', color: 'black', fontSize: '11px' }}>Los precios no incluyen traslado</span> <br/>
                 <span   style={{textAlign: 'left', color: 'black', fontSize: '10px' }}>Los precios son validos por 30 dias a partir de la fecha de emision : {hoy}/{mesActual}/{year} </span> <br/>
                 <p className="mb-2"> <a className="fw-semi-bold"  href={url}> Coa Revestimientos</a>
